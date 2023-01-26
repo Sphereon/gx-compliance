6c282b2914fb32f5ddb601953325339e16e218e9
sksadjad <skhoshkhou@sphereon.com>
2023-01-26 08:55:38 +0100

    fix: reverted the schema change in creating complianceCredential

--- src/common/services/signature.service.ts
+++ src/common/services/signature.service.ts
@@ -132,10 +132,7 @@ export class SignatureService {
     const complianceCredentialType: string =
       SelfDescriptionTypes.PARTICIPANT === type ? SelfDescriptionTypes.PARTICIPANT_CREDENTIAL : SelfDescriptionTypes.SERVICE_OFFERING_CREDENTIAL
     const unsignedCredential: ICredential = {
-      '@context': [
-        'https://www.w3.org/2018/credentials/v1',
-        'https://github.com/Sphereon/gx-compliance/blob/feature/FMA-7/src/static/schemas/gaia-x'
-      ],
+      '@context': ['https://www.w3.org/2018/credentials/v1'],
       type: ['VerifiableCredential', complianceCredentialType],
       id: `https://catalogue.gaia-x.eu/credentials/${complianceCredentialType}/${new Date().getTime()}`,
       issuer: getDidWeb(),

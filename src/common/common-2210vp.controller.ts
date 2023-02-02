515b9c46c12fad1ce111a2b2ca24b1b7096cec3e
sksadjad <skhoshkhou@sphereon.com>
2023-02-02 11:59:38 +0100

    chore: reverted the common controlller changes

---
+++ src/common/common-2210vp.controller.ts
@@ -0,0 +1,84 @@
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Post, UsePipes } from '@nestjs/common'
+import { ProofService, SelfDescriptionService, SignatureService } from './services'
+import { ParticipantSelfDescriptionDto } from '../participant/dto'
+import { ServiceOfferingSelfDescriptionDto } from '../service-offering/dto'
+import { VerifiableCredentialDto } from './dto'
+import ParticipantSD from '../tests/fixtures/participant-sd.json'
+import PresentationExample from '../tests/fixtures/sphereon-presentation.json'
+import CredentialExample from '../tests/fixtures/sphereon-credential.json'
+import ServiceOfferingExperimentalSD from '../tests/fixtures/service-offering-sd.json'
+import { JoiValidationPipe } from './pipes'
+import { VerifiablePresentationSchema } from './schema/selfDescription.schema'
+import { CredentialTypes } from './enums'
+import { getTypeFromSelfDescription } from './utils'
+import { VerifiablePresentationDto } from './dto/presentation-meta.dto'
+import { IVerifiableCredential } from './@types/SSI.types'
+
+const credentialType = CredentialTypes.common
+
+const commonSDExamples = {
+  participant: { summary: 'Participant SD Example', value: ParticipantSD.selfDescriptionCredential },
+  service: { summary: 'Service Offering Experimental SD Example', value: ServiceOfferingExperimentalSD.selfDescriptionCredential }
+}
+
+@ApiTags(credentialType)
+@Controller({ path: '' })
+export class CommonController {
+  constructor(
+    private readonly selfDescriptionService: SelfDescriptionService,
+    private readonly signatureService: SignatureService,
+    private readonly proofService: ProofService
+  ) {}
+
+  @ApiResponse({
+    status: 201,
+    description: 'Successfully signed posted content. Will return the posted JSON with an additional "proof" property added.'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid JSON request body.'
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Invalid Participant Self Description.'
+  })
+  @ApiBody({
+    type: VerifiablePresentationDto,
+    //fixme: create examples for compliance
+    examples: commonSDExamples
+  })
+  @ApiOperation({ summary: 'Gets a selfDescribed VP and returns a Compliance VC in response' })
+  @UsePipes(new JoiValidationPipe(VerifiablePresentationSchema))
+  @Post('compliance')
+  async createComplianceCredential(@Body() verifiableSelfDescription: VerifiablePresentationDto): Promise<IVerifiableCredential> {
+    const sd = JSON.parse(JSON.stringify(verifiableSelfDescription))
+    await this.proofService.validate(sd)
+    const type: string = getTypeFromSelfDescription(sd.verifiableCredential[0])
+
+    await this.selfDescriptionService.validateSelfDescription(sd, type)
+    return await this.signatureService.createComplianceCredentialFromSelfDescription(sd)
+  }
+
+  @Post('normalize')
+  @ApiResponse({
+    status: 201,
+    description: 'Normalized Self Description.'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request.'
+  })
+  @ApiOperation({ summary: 'Normalize (canonize) a Self Description using URDNA2015' })
+  @ApiBody({
+    type: VerifiableCredentialDto,
+    examples: commonSDExamples
+  })
+  async normalizeSelfDescriptionRaw(
+    @Body() selfDescription: VerifiableCredentialDto<ParticipantSelfDescriptionDto | ServiceOfferingSelfDescriptionDto>
+  ): Promise<string> {
+    const normalizedSD: string = await this.signatureService.normalize(selfDescription)
+
+    return normalizedSD
+  }
+}
--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,19 +1,15 @@
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { ApiBody, ApiResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Body, Controller, Post, UsePipes } from '@nestjs/common'
-import { ProofService, SelfDescriptionService, SignatureService } from './services'
+import { SignatureService, SelfDescriptionService, ProofService } from './services'
 import { ParticipantSelfDescriptionDto } from '../participant/dto'
 import { ServiceOfferingSelfDescriptionDto } from '../service-offering/dto'
 import { ComplianceCredentialDto, VerifiableCredentialDto } from './dto'
 import ParticipantSD from '../tests/fixtures/participant-sd.json'
-import PresentationExample from '../tests/fixtures/sphereon-presentation.json'
-import CredentialExample from '../tests/fixtures/sphereon-credential.json'
 import ServiceOfferingExperimentalSD from '../tests/fixtures/service-offering-sd.json'
 import { JoiValidationPipe } from './pipes'
-import { ParticipantSelfDescriptionSchema, VerifiablePresentationSchema } from './schema/selfDescription.schema'
-import { CredentialTypes, SelfDescriptionTypes } from './enums'
+import { ParticipantSelfDescriptionSchema } from './schema/selfDescription.schema'
+import { CredentialTypes } from './enums'
 import { getTypeFromSelfDescription } from './utils'
-import { VerifiablePresentationDto } from './dto/presentation-meta.dto'
-import { IVerifiableCredential } from './@types'
 
 const credentialType = CredentialTypes.common
 
@@ -21,20 +17,14 @@ const commonSDExamples = {
   participant: { summary: 'Participant SD Example', value: ParticipantSD.selfDescriptionCredential },
   service: { summary: 'Service Offering Experimental SD Example', value: ServiceOfferingExperimentalSD.selfDescriptionCredential }
 }
-const credentialExample = {
-  participant: { summary: 'A sample participant credential ready to be signed', value: CredentialExample }
-}
-const presentationExample = {
-  participant: { summary: 'A sample participant presentation ready to be signed', value: PresentationExample }
-}
 @ApiTags(credentialType)
 @Controller({ path: '' })
 export class CommonController {
   constructor(
-    private readonly selfDescriptionService: SelfDescriptionService,
-    private readonly signatureService: SignatureService,
-    private readonly proofService: ProofService
-  ) {}
+      private readonly selfDescriptionService: SelfDescriptionService,
+      private readonly signatureService: SignatureService,
+      private readonly proofService: ProofService
+  ) { }
 
   @ApiResponse({
     status: 201,
@@ -56,46 +46,17 @@ export class CommonController {
   @UsePipes(new JoiValidationPipe(ParticipantSelfDescriptionSchema))
   @Post('sign')
   async signSelfDescription(
-    @Body() verifiableSelfDescription: VerifiableCredentialDto<ParticipantSelfDescriptionDto | ServiceOfferingSelfDescriptionDto>
+      @Body() verifiableSelfDescription: VerifiableCredentialDto<ParticipantSelfDescriptionDto | ServiceOfferingSelfDescriptionDto>
   ): Promise<{ complianceCredential: VerifiableCredentialDto<ComplianceCredentialDto> }> {
     await this.proofService.validate(JSON.parse(JSON.stringify(verifiableSelfDescription)))
     const type: string = getTypeFromSelfDescription(verifiableSelfDescription)
 
     await this.selfDescriptionService.validateSelfDescription(verifiableSelfDescription, type)
     const complianceCredential: { complianceCredential: VerifiableCredentialDto<ComplianceCredentialDto> } =
-      await this.signatureService.createComplianceCredential(verifiableSelfDescription)
+        await this.signatureService.createComplianceCredential(verifiableSelfDescription)
 
     return complianceCredential
   }
-
-  @ApiResponse({
-    status: 201,
-    description: 'Successfully signed posted content. Will return the posted JSON with an additional "proof" property added.'
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Invalid JSON request body.'
-  })
-  @ApiResponse({
-    status: 409,
-    description: 'Invalid Participant Self Description.'
-  })
-  @ApiBody({
-    type: VerifiablePresentationDto,
-    examples: commonSDExamples
-  })
-  @ApiOperation({ summary: 'Gets a selfDescribed VP and returns a Compliance VC in response' })
-  @UsePipes(new JoiValidationPipe(VerifiablePresentationSchema))
-  @Post('compliance')
-  async createComplianceCredential(@Body() verifiableSelfDescription: VerifiablePresentationDto): Promise<IVerifiableCredential> {
-    const sd = JSON.parse(JSON.stringify(verifiableSelfDescription))
-    await this.proofService.validate(sd)
-    const type: string = getTypeFromSelfDescription(sd.verifiableCredential[0])
-
-    await this.selfDescriptionService.validateSelfDescription(sd, type)
-    return await this.signatureService.createComplianceCredentialFromSelfDescription(sd)
-  }
-
   @Post('normalize')
   @ApiResponse({
     status: 201,
@@ -111,7 +72,7 @@ export class CommonController {
     examples: commonSDExamples
   })
   async normalizeSelfDescriptionRaw(
-    @Body() selfDescription: VerifiableCredentialDto<ParticipantSelfDescriptionDto | ServiceOfferingSelfDescriptionDto>
+      @Body() selfDescription: VerifiableCredentialDto<ParticipantSelfDescriptionDto | ServiceOfferingSelfDescriptionDto>
   ): Promise<string> {
     const normalizedSD: string = await this.signatureService.normalize(selfDescription)
 

ef5950d1b97c260e40cefd5b9f7361a8877dcfec
sksadjad <skhoshkhou@sphereon.com>
2023-02-02 16:04:03 +0100

    chore: reverted self-description util

--- src/common/utils/self-description.util.ts
+++ src/common/utils/self-description.util.ts
@@ -1,17 +1,13 @@
-import { VerifiableCredentialDto } from '../dto'
+import { CredentialSubjectDto, VerifiableCredentialDto } from '../dto'
 import { SUPPORTED_TYPES } from '../constants'
 import { BadRequestException, ConflictException } from '@nestjs/common'
 
-export function getTypeFromSelfDescription(selfDescription: VerifiableCredentialDto<any>): string {
-  const sdTypes = selfDescription.type
-  if (!sdTypes) throw new BadRequestException('Expected type to be defined in Self Description')
+export function getTypeFromSelfDescription(selfDescription: VerifiableCredentialDto<CredentialSubjectDto>): string {
+  const types = selfDescription.type
+  if (!types) throw new BadRequestException('Expected type to be defined in Self Description')
 
-  const types = sdTypes.filter(type => SUPPORTED_TYPES.includes(type))
-  if (types.length === 0) {
-    throw new ConflictException('Provided type for Self Description is not supported')
-  } else if (types.length > 1) {
-    throw new ConflictException('Multiple provided types for for a Self Description are not supported')
-  }
+  const type: string = types.find(t => t !== 'VerifiableCredential')
+  if (!SUPPORTED_TYPES.includes(type)) throw new ConflictException('Provided type for Self Description is not supported')
 
-  return types[0]
+  return type
 }

c41b98a89fd05114376157e69ac11d4d428cab0e
nklomp <nklomp@sphereon.com>
2023-02-14 12:20:42 +0100

    fix: we were not using the 2210vp did utils

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
 import { RequestMethod } from '@nestjs/common'
 import { setupSwagger } from './swagger'
-import { createDidDocument } from './utils/methods/did.util'
+import { createDidDocument } from './utils/methods/did.2210vp.util'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
--- src/utils/methods/did.2210vp.util.ts
+++ src/utils/methods/did.2210vp.util.ts
@@ -3,14 +3,13 @@ import * as jose from 'jose'
 import { join } from 'path'
 
 export const X509_VERIFICATION_METHOD_NAME = 'X509-JWK2020'
-export const DID_DOC_FILE_PATH = join(__dirname, '../../static/.well-known/did.json')
-export const X509_CERTIFICATE_CHAIN_FILE_PATH = join(__dirname, '../../static/.well-known/x509CertificateChain.pem')
+export const DID_DOC_FILE_PATH = join(__dirname, '../static/.well-known/did.json')
+export const X509_CERTIFICATE_CHAIN_FILE_PATH = join(__dirname, '../static/.well-known/x509CertificateChain.pem')
 
 export function getDidWeb() {
-  return 'did:web:e92e-87-213-241-251.eu.ngrok.io'
-  /*return `did:web:${getBaseUrl()
+  return `did:web:${getBaseUrl()
     .replace(/https?:\/\//, '')
-    .replace('/', ':')}`*/
+    .replace('/', ':')}`
 }
 
 export function getBaseUrl() {

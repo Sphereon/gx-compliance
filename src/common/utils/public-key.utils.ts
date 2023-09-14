8b5bb9b0b9e0a6b85109c8b4d534c4a8f9a4c1de
sander <spostma@sphereon.com>
2023-09-14 17:33:36 +0200

    Improved error handling

--- src/common/utils/public-key.utils.ts
+++ src/common/utils/public-key.utils.ts
@@ -1,9 +1,20 @@
-import { writeFileSync } from 'fs'
-import { join } from 'path'
+import {writeFileSync} from 'fs'
+import {join} from 'path'
+
+function parseCertificate() {
+    const certificateData = process.env.X509_CERTIFICATE;
+
+    // Check if the certificate data contains "-----"
+    if (certificateData.includes("-----")) {
+        return certificateData;
+    } else {
+        return atob(certificateData);
+    }
+}
 
 export function importCertChain() {
-  if (!!process.env.X509_CERTIFICATE) {
-    const X509_CERTIFICATE_CHAIN_FILE_PATH = join(__dirname, '../../static/.well-known/x509CertificateChain.pem')
-    writeFileSync(X509_CERTIFICATE_CHAIN_FILE_PATH, process.env.X509_CERTIFICATE)
-  }
+    if (!!process.env.X509_CERTIFICATE) {
+        const X509_CERTIFICATE_CHAIN_FILE_PATH = join(__dirname, '../../static/.well-known/x509CertificateChain.pem')
+        writeFileSync(X509_CERTIFICATE_CHAIN_FILE_PATH, parseCertificate())
+    }
 }
